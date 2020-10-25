5b7047082f23fe99c2bf89b4f30558d8cc6a8886
GersonGMR <56897299+GersonGMR@users.noreply.github.com>
2020-10-25 14:23:49 -0600

    Creando menú principal en modo consola

--- lab2.py
+++ lab2.py
@@ -5,3 +5,9 @@ ejecucion = time.time()
 
 with open('file.txt') as el_archivo:
     leyendo_archivo = list(map(int, el_archivo.read().split('\n')))
+
+def mi_menu():
+    print("[1] Ingrese el nombre del archivo a analizar")
+    print("[0] Terminar")
+mi_menu()
+opcion_menu = int (input("Elija una opción del menu: "))
