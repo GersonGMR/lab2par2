d7e11495d40fee9aaf37bce84c76e2d51adc6203
GersonGMR <56897299+GersonGMR@users.noreply.github.com>
2020-10-25 14:29:57 -0600

    Rediseñando menu con validaciones

--- lab2.py
+++ lab2.py
@@ -7,7 +7,33 @@ with open('file.txt') as el_archivo:
     leyendo_archivo = list(map(int, el_archivo.read().split('\n')))
 
 def mi_menu():
-    print("[1] Ingrese el nombre del archivo a analizar")
-    print("[0] Terminar")
-mi_menu()
-opcion_menu = int (input("Elija una opción del menu: "))
+ 
+    opcion_correcta = False
+    num = 0
+    while(not opcion_correcta):
+        try:
+            num = int(input("Introduce un numero entero del menu: "))
+            opcion_correcta = True
+        except ValueError:
+            print('Error, introduce un numero entero que se encuentre en el menu')
+    return num
+ 
+salir = False
+opcion = 0
+ 
+while not salir:
+ 
+    print ("1. Ingresar el nombre del archivo de texto para analizar")
+    print ("2. Salir")
+    print ("Elige una opcion")
+ 
+    opcion = mi_menu()
+ 
+    if opcion == 1:
+        print ("Opcion 1")
+    elif opcion == 2:
+        salir = True
+    else:
+        print ("Introduce un numero entre 1 y 2")
+ 
+print ("Finalizado")
