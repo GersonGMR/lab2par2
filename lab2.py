1db5f4e9247983cf513e4a7fb2efafd34f06629c
GersonGMR <56897299+GersonGMR@users.noreply.github.com>
2020-10-25 18:55:56 -0600

    Pruebas finales contando palabras unicas

--- lab2.py
+++ lab2.py
@@ -1,43 +1,42 @@
 import time
-import numpy as np
-import glob
-
-ejecucion = time.time()
-
-global nombre_archivo
 
 def analisar_archivo():
+    # Iniciando tiempo de ejecucion del programa
+    ejecucion = time.time()
+
+    # Asignando a la variable path mi ruta del proyecto
     path = "C:/Users/GersonGM/github/lab2par2SH/"
+
+    # Leyendo los datos del archivo proporcionado
     leyendo_archivo = open(path+nombre_archivo, "r")
     datos = leyendo_archivo.read()
 
+    # Convertiendo nuestros datos del archivo a string
     conversion_datos = str(datos)
     mis_datos = conversion_datos
 
+    # Separando las palabras para poder realizar su conteo
     separando_palabras = datos.split()
     cantidad_palabras = len(separando_palabras)
     cantidad_caracteres = len(mis_datos)
 
+    # Realizando conteo de lineas para sumarlas
     leyendo_archivo.readline()
     leyendo_archivo.seek(0)
     cantidad_lineas = len(leyendo_archivo.readlines())
-
-    leyendo_archivo.close()
-
-    for i in range(0,cantidad_caracteres,1):
-        if ((ord(mis_datos[i]) >= 97) and (ord(mis_datos[i]) <= 122)):
-            letras_minusculas=letras_minusculas+1
-        elif ((ord(mis_datos[i]) >= 65) and (ord(mis_datos[i]) <= 90)):
-            letras_mayusculas+=1
-        elif ((ord(mis_datos[i]) >= 48) and (ord(mis_datos[i]) <= 57)):
-            digitos+=1
-        elif (ord(mis_datos[i])==32):
-            espacios+=1
-        else:
-            others+=1
     
-    print("Cantidad de palabras: %d \n Cantidad de caracteres: %d \n Cantidad de lineas: %d \n" 
-    %(cantidad_palabras,cantidad_caracteres,cantidad_lineas))
+    # Realizando separacion de palabras unicas
+    palabras_unicas = set()
+    for filas in datos:
+        palabras_unicas |= set(filas.split())
+
+    contador_palabras_unicas = len(palabras_unicas)    
+
+    # Cerrando el archivo leido anteriormente
+    leyendo_archivo.close()
+    
+    print("Cantidad de palabras: %d \n Cantidad de caracteres: %d \n Cantidad de lineas: %d \n Cantidad de palabras unicas: %d \n" %(cantidad_palabras,cantidad_caracteres,cantidad_lineas,contador_palabras_unicas))
+    print('Duracion: {} segundos'.format(time.time() - ejecucion))
 
 
 def mi_menu():
